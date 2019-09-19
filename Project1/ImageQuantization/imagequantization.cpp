92611e663928930d4cf5c8dad105a191cb009684
Guillermo Del Valle <gdelvalle@nevada.unr.edu>
2019-09-18 19:25:44 -0700

    Quantization donegit add .

---
+++ Project1/ImageQuantization/imagequantization.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include "stdlib.h"
+
+#include "image.h"
+using namespace std;
+
+int readImageHeader(char[], int&, int&, int&, bool&);
+int readImage(char[], ImageType&);
+int writeImage(char[], ImageType&);
+void quantization(ImageType, ImageType&, int);
+void upscale(ImageType, ImageType&, int);
+
+int main(int argc, char *argv[]){
+    int M, N, Q;
+    bool type;
+    int factor = argv[3][0] - '0';
+    // read image header
+    readImageHeader(argv[1], N, M, Q, type);
+
+    // allocate memory for the image array
+    ImageType image(N, M, Q);
+
+    // read image
+    readImage(argv[1], image);
+
+    // write image
+    ImageType newimage(N, M, Q);
+    quantization(image, newimage, factor);
+
+    writeImage(argv[2], newimage);
+
+    return (1);
+
+}
+
+void quantization(ImageType ogImage, ImageType& newimage, int quantization_factor){
+    int rows = 0;
+    int cols = 0;
+    int levels = 0;
+    int new_level = 256 / quantization_factor;
+
+    ogImage.getImageInfo(rows, cols, levels);
+
+    int val = 0;
+    for(int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+            ogImage.getPixelVal(i, j, val);
+            val = floor((double)val/new_level)*new_level;
+            newimage.setPixelVal(i, j, val);
+        }
+    }
+    //cout<<"done"<<endl;
+}
