38da57acfb09ac4f9fd07205954d55fff9e2fc36
Guillermo Del Valle <gdelvalle@nevada.unr.edu>
2019-10-17 12:09:52 -0700

    matt came in to save the day

--- Project2/sharpening/sharpening.cpp
+++ Project2/sharpening/sharpening.cpp
@@ -12,52 +12,22 @@ using namespace std;
 int readImageHeader(char[], int&, int&, int&, bool&);
 int readImage(char[], ImageType&);
 int writeImage(char[], ImageType&);
-int applyMask(int, int, int**);
-void filter(ImageType, ImageType&, int**, int);
-void normalizeImage(ImageType, ImageType&);
-int** subimage(ImageType, int, int, int);
+
+void gradientMagn(ImageType, ImageType, ImageType&);
+void filter(ImageType img, ImageType& filterimg, int filtersize, int N, int M, int mask[]);
 
 int main(int argc, char *argv[]){
     int M, N, Q;
     int mask_size = 3;
     bool type;
-    int** h_prewitt = 0;
-    int** v_prewitt = 0;
-    int** h_sobel = 0;
-    int** v_sobel = 0;
-    int** laplacian = 0;
-    h_prewitt = new int*[mask_size];
-    v_prewitt = new int*[mask_size];
-    h_sobel = new int*[mask_size];
-    v_sobel = new int*[mask_size];
-    laplacian = new int*[mask_size];
-    for(int x = 0; x < mask_size; x++){
-      h_prewitt[x] = new int[mask_size];
-      v_prewitt[x] = new int[mask_size];
-      h_sobel[x] = new int[mask_size];
-      v_sobel[x] = new int[mask_size];
-      laplacian[x] = new int[mask_size];
-    }
+    int v_prewitt[9] = {-1,0,1,-1,0,1,-1,0,1};
+    int h_prewitt[9] = {-1,-1,-1,0,0,0,1,1,1};
+    int v_sobel[9] = {-1,-2,-1,0,0,0,1,2,1};
+    int h_sobel[9] = {-1,0,1,-2,0,2,-1,0,2};
+    int laplacian[9] = {0,1,0,1,-4,1,0,1,0};
 
-    h_prewitt[0][0] = -1, h_prewitt[0][1] = 0, h_prewitt[0][2] = 1;
-    h_prewitt[1][0] = -1, h_prewitt[1][1] = 0, h_prewitt[1][2] = 1;
-    h_prewitt[2][0] = -1, h_prewitt[2][1] = 0, h_prewitt[2][2] = 1;
 
-    v_prewitt[0][0] = -1, v_prewitt[0][1] = -1, v_prewitt[0][2] = -1;
-    v_prewitt[1][0] = 0, v_prewitt[1][1] = 0, v_prewitt[1][2] = 0;
-    v_prewitt[2][0] = 1, v_prewitt[2][1] = 1, v_prewitt[2][2] = 1;
 
-    h_sobel[0][0] = -1, h_sobel[0][1] = 0, h_sobel[0][2] = 1;
-    h_sobel[1][0] = -2, h_sobel[1][1] = 0, h_sobel[1][2] = 2;
-    h_sobel[2][0] = -1, h_sobel[2][1] = 0, h_sobel[2][2] = 1;
-
-    v_sobel[0][0] = -1, v_sobel[0][1] = -2, v_sobel[0][2] = -1;
-    v_sobel[1][0] = 0, v_sobel[1][1] = 0, v_sobel[1][2] = 0;
-    v_sobel[2][0] = 1, v_sobel[2][1] = 2, v_sobel[2][2] = 1;
-
-    laplacian[0][0] = 0, laplacian[0][1] = 1, laplacian[0][2] = 0;
-    laplacian[1][0] = 1, laplacian[1][1] = -4, laplacian[1][2] = 1;
-    laplacian[2][0] = 0, laplacian[2][1] = 1, laplacian[2][2] = 0;
     // read image header
     readImageHeader(argv[1], N, M, Q, type);
     // allocate memory for the image array
@@ -71,105 +41,73 @@ int main(int argc, char *argv[]){
     ImageType hpimg(N,M,Q);
     ImageType vpimg(N,M,Q);
     ImageType lapimg(N,M,Q);
+    ImageType gradS(N,M,Q);
+    ImageType gradP(N,M,Q);
 
-    filter(image, hsimg, h_sobel, mask_size);
-    filter(image, vsimg, v_sobel, mask_size);
-    filter(image, hpimg, h_prewitt, mask_size);
-    filter(image, vpimg, v_prewitt, mask_size);
-    filter(image, lapimg, laplacian, mask_size);
-    writeImage(argv[2], hsimg);
-    writeImage(argv[3], vsimg);
-    writeImage(argv[4], hpimg);
-    writeImage(argv[5], vpimg);
+    filter(image, vsimg,mask_size, N, M, v_sobel);
+    filter(image, hsimg,mask_size,N,M,h_sobel);
+    filter(image, vpimg,mask_size,N,M,v_prewitt);
+    filter(image, hpimg,mask_size,N,M, h_prewitt);
+    filter(image, lapimg,mask_size,N,M, laplacian);
+
+
+    gradientMagn(hsimg,vsimg,gradS);
+    gradientMagn(hpimg,vpimg,gradP);
+    writeImage(argv[2], vsimg);
+    writeImage(argv[3], hsimg);
+    writeImage(argv[4], vpimg);
+    writeImage(argv[5], hpimg);
     writeImage(argv[6], lapimg);
+    writeImage(argv[7], gradS);
+    writeImage(argv[8], gradP);
 
     return (1);
 
 }
 
-int applyMask(int** pixels, int mask_size, int** mask){
-  int sum = 0, temp = 0;
-  for(int i = 0; i < mask_size; i++){
-    for(int j = 0; j < mask_size; j++){
-      temp = mask[i][j] * pixels[i][j];
-      sum += temp;
-    }
-  }
-  return sum;
-}
 
-void filter(ImageType oldImg, ImageType& newImg, int** mask, int mask_size){
-  int M, N, Q;
-  oldImg.getImageInfo(N,M,Q);
+void gradientMagn(ImageType x, ImageType y, ImageType& newi)
+{
+  int xpix;
+  int ypix;
+  int square;
+  int N, M, Q;
+  x.getImageInfo(N,M,Q);
   for(int i = 0; i < N; i++){
     for(int j = 0; j < M; j++){
-      int** sub = subimage(oldImg, i, j, mask_size);
-      int sum = applyMask(sub, mask_size, mask);
-      newImg.setPixelVal(i,j,sum);
+      x.getPixelVal(i,j,xpix);
+      y.getPixelVal(i,j,ypix);
+      square = sqrt((xpix * xpix) + (ypix * ypix));
+      newi.setPixelVal(i,j,square);
     }
   }
 }
+void filter(ImageType img, ImageType& filterimg, int filtersize, int N, int M, int mask[]){
+    float applied = 0;
+    int curr_mask = 0;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            for(int s = (-filtersize/2); s <= (filtersize/2); s++){
+                for(int t = (-filtersize/2); t <= (filtersize/2); t++){
+                    int value = 0;
+                    if(((i+s)<0 || (i+s)>=N) || ((j+t)<0 || (j+t)>=M)){
+                        applied+=0;
+                    }
+                    else{
+                        img.getPixelVal(i+s, j+t, value);
+                        //cout<<value<<" "<<mask[curr_mask]<<endl;
+                        applied += (value*mask[curr_mask]);
 
-int** subimage(ImageType img, int x, int y, int mask_size){
-  int** sub = 0;
-  sub = new int*[mask_size];
-  int M = 0;
-  int N = 0;
-  int Q = 255;
-  int x_count = 0;
-  int y_count = 0;
-  int pixel = 0;
-  img.getImageInfo(N,M,Q);
-  //cout << n_mask << endl;
-  //cout << m_mask << endl;
-  for(int k = 0; k < mask_size; k++){
-    sub[k] = new int[mask_size];
-  }
-  for(int i = -mask_size/2; i<mask_size/2; i++){
-    for(int j = -mask_size/2; j< mask_size/2; j++){
-  //    cout << y_count << " " << x_count << endl;
-      if((x+i>=0 && x+i<N) && (y+j>=0 && y+j<M)){
-        img.getPixelVal(x+i, y+j, pixel);
-        sub[x_count][y_count] = pixel;
-      }
-      else{
-        sub[x_count][y_count] = 0;
-      }
-      y_count++;
-    }
-    y_count = 0;
-    x_count++;
-}
+                    }
+                    curr_mask++;
+                }
+            }
+            //cout<<applied<<endl;
+            filterimg.setPixelVal(i, j, applied);
 
-  return sub;
-}
+            applied = 0;
+            curr_mask = 0;
+        }
 
-void normalizeImage(ImageType ogImage, ImageType& newImage){
-  int M = 0;
-  int N = 0;
-  int Q = 0;
-  int x = 0;
-  int counter = 0;
-  double newPix = 0;
-  int pix = 0;
-  ogImage.getImageInfo(N, M, Q);
-  int max = 0;
-  int min = 1000;
-  for(int i = 0; i < N; i++){
-    for(int j = 0; j < M; j++){
-      ogImage.getPixelVal(i,j,x);
-      max = std::max(max, x);
-      min = std::min(min, x);
     }
-  }
-  cout << max << " " << min << endl;
-  for(int k = 0; k < N; k++){
-    for(int l = 0; l < M; l++){
-      ogImage.getPixelVal(k,l,x);
-      newPix = (double)(x*255)/(double)max;
-      pix = (int)newPix;
-      newImage.setPixelVal(k,l,pix);
-    }
-  }
-//  cout << counter << endl;
 }
